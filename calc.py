2ec911c04fac4f12934f149e6d99f88da99b860e
Kleveriwe <noobsaibotterrach@ya.ru>
2025-07-29 16:15:53 +0300

    Шота сделал

--- calc.py
+++ calc.py
@@ -1,41 +1,33 @@
-def calc(a  : int, operator: str, b:int)-> int:
-    try:
-        if operator == '+':
-            return a + b
-        elif operator == '-':
-            return a - b
-        elif operator == '*':
-            return a * b
-        elif operator == '/':
-            if b == 0:
-                raise ZeroDivisionError('на ноль делить нельзя')
+import argparse
+import sys
+
+
+def calc(a: float, operator: str, b: float) -> float:
+    if operator == '+':
+        return a + b
+    elif operator == '-':
+        return a - b
+    elif operator == '*':
+        return a * b
+    elif operator == '/':
+        if b != 0:
             return a / b
         else:
-            return f'Ошибка: неподдерживаемый оператор {operator}'
+            raise ValueError("Деление на ноль!")
+    else:
+        raise ValueError(f"Неизвестный оператор: {operator}")
 
-    except ValueError:
-        return 'Ошибка: a и b должны быть числами.'
 
-    except SyntaxError:
-        print('Ошибка: оператор должен быть строкой (например, "+", "-")')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Простой калькулятор. Принимает два числа и оператор, пример: 2 + 2")
+    parser.add_argument('a', type=float, help="Первое число")
+    parser.add_argument('operator', choices=['+', '-', '*', '/'], help="Оператор (+, -, *, /)")
+    parser.add_argument('b', type=float, help="второе число")
+    args = parser.parse_args()
 
-    except Exception as e: # этот отбор из gpt взял, но с ним лучше
-        return f'Ошибка: {e}'
-
-# ВОПРОС, ты в условии её не прописывал, но как мне кажется нужно перехватить еще ошибку, если оператор не str
-# (БЕЗ КАВЫЧЕК)
-# print(calc(5,/,8))
-# но если я прописываю
-# if not isinstance(operator, str):
-#             return "Ошибка: оператор должен быть строкой (например, '+', '-')"
-# или
-# except SyntaxError:
-#         print('Ошибка: оператор должен быть строкой (например, "+", "-")')
-# она не уходит
-# и вот эта gpt строчка (    except Exception as e:
-#         return f'Ошибка: {e}')
-#
-#         как я понял вообще должна перехватывать все иные ошибки,
-# но эта все равно вылетает в терминале
-# её можно как-то нашаманить ?
-# и нужно ли ахах?
+    try:
+        result = calc(args.a, args.operator, args.b)
+    except ValueError as e:
+        print(f"Ошибка: {e}", file=sys.stderr)
+        sys.exit(2)
+    print(result)
